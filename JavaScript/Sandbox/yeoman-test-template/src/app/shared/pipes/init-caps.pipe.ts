33326c74e5ccad9f27fb8b1dbd0b162584c01e20
Vladimir Petrov <vpetrov@lohika.com>
2017-03-13 00:54:30 +0200

    Removed unnecessary test projects

--- JavaScript/Sandbox/yeoman-test-template/src/app/shared/pipes/init-caps.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from 'angular2/core';
-
-@Pipe({ name: 'initCaps' })
-export class InitCaps implements PipeTransform {
-  transform(value: number, exponent: string): number {
-    let exp = parseFloat(exponent);
-    return Math.pow(value, isNaN(exp) ? 1 : exp);
-  }
-}
