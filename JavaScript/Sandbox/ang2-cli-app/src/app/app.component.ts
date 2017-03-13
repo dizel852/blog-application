cfa6a3dabd03c216f1e346cc9fe62dca125e60c6
Vladimir Petrov <vpetrov@lohika.com>
2017-03-13 02:58:03 +0200

    [ang2-cli-app] Added few features

--- JavaScript/Sandbox/ang2-cli-app/src/app/app.component.ts
+++ JavaScript/Sandbox/ang2-cli-app/src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';  
+import { BrowserModule } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Ultra Racing';
+  carParts = [{
+    "id": 1,
+    "name": "Super Tires",
+    "description": "These tires are the very best",
+    "inStock": 5,
+    "available": true
+
+  },
+  {
+    "id": 2,
+    "name": "Cool Tires",
+    "description": "These tires are the very best",
+    "inStock": 2,
+    "available": true
+
+  },
+  {
+    "id": 3,
+    "name": "Expensive Tires",
+    "description": "These tires are the very best",
+    "inStock": 6,
+    "available": false
+
+  },
+  {
+    "id": 4,
+    "name": "Mega Tires",
+    "description": "These tires are the very best",
+    "inStock": 3,
+    "available": true
+
+  }
+  ]
 }
