375cdc747e4990c9d3849bf15d85c87094ade853
Vladimir Petrov <vladimirp@hpe.com>
2017-03-16 12:14:20 +0200

    [ang2-cli-blog-app] Added toogle navigation for mobile view

--- JavaScript/Sandbox/ang2-cli-blog-app/src/app/header.component.ts
+++ JavaScript/Sandbox/ang2-cli-blog-app/src/app/header.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule  } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class HeaderComponent {
+   isIn = false; // store state
+   toggleState() { // click handler
+        let bool = this.isIn;
+        this.isIn = bool === false ? true : false; 
+    }
 }
